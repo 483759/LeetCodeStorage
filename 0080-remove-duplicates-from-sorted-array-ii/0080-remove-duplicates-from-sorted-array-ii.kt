8c0e009ed865fa784f0c08b4bd0626e2d52de92f
IJin Yun <483759@naver.com>
2026-01-25 18:03:04 +0900

    Time: 193 ms (17.16%), Space: 49.1 MB (33.99%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.kt
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.kt
@@ -1,15 +1,11 @@
 class Solution {
     fun removeDuplicates(nums: IntArray): Int {
-        val frequency = mutableMapOf<Int, Int>()
-        nums.forEach { frequency[it] = frequency.getOrDefault(it, 0) + 1 }
-
-        var index = 0
-        frequency.forEach {
-            nums[index++] = it.key
-            if (it.value > 1) {
-                nums[index++] = it.key
+        if (nums.isEmpty()) return 0
+        var i = 2
+        for (j in 2 until nums.size)
+            if (nums[j] != nums[i - 2]) {
+                nums[i++] = nums[j]
             }
-        }
-        return index
+        return i
     }
 }
